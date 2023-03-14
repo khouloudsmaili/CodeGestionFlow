c3bcb3efe991b3caed1e1b2dc70682ca4f6a1ea6
Khouloud Smaili <khouloudsamili@gmail.com>
2023-03-14 13:14:59 +0100

    Hello world programm

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+// C'est une fonction d'initialisation
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.print("Bonjour");
+}
+
+// C'est une fonction exécutée toujours
+void loop() {
+  // put your main code here, to run repeatedly:
+}
